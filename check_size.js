85a693381bef768c0eb910d13d4a2756b8b2687c
Dooley <dooley@example.com>
2026-04-22 19:20:49 -0700

    commit changes save changes

--- check_size.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('test_csg_data.txt', 'utf8');
-console.log(data.length);
--- check_size2.cjs
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('test_csg_data2.txt', 'utf8');
-console.log(data.length);
